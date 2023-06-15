15448ebcacab0c689eba75f69916a52581ca62d1
Mahabub Alam <rjmahabub543@gmail.com>
2023-06-16 00:26:50 +0600

    [DONE] /blog/dynamic route page right side bar

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 import BlogLikeCommentSide from "../../../../components/pages/Blog/BlogLikeCommentSide";
 import BlogMainContent from "../../../../components/pages/Blog/BlogMainContent";
+import BlogRIghtSIdeMoreBlogMenu from "../../../../components/pages/Blog/BlogRIghtSIdeMoreBlogMenu";
+import BlogRIghtUserCart from "../../../../components/pages/Blog/BlogRIghtUserCart";
 
 const page = ({ params }: { params: { id: string } }) => {
     return (
@@ -8,21 +10,22 @@ const page = ({ params }: { params: { id: string } }) => {
             className="container grid grid-cols-12 gap-main-content-gap mt-main-content-gap"
         >
             <div
-                className="col-span-1  flex flex-col items-center gap-6 mt-10"
+                className="col-span-1 flex flex-col items-center gap-6 mt-10"
             >
                 <BlogLikeCommentSide
                     dynamicID={params?.id}
                 />
             </div>
             <div
-                className="col-span-8 box_area space-y-main-content-gap"
+                className="col-span-8 space-y-main-content-gap bg-primary dark:bg-darkMode-contentBG border box-border-color rounded-common overflow-hidden pb-common"
             >
                 <BlogMainContent />
             </div>
             <div
-                className="col-span-3 box_area h-80"
+                className="col-span-3 space-y-common-.75"
             >
-                <p>this is daynamin page</p>
+                <BlogRIghtUserCart />
+                <BlogRIghtSIdeMoreBlogMenu />
             </div>
         </section>
     );
