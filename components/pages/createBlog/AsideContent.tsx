c4070b33d633d2d23656f17b01a132f32bd8a6bc
Mahabub Alam <rjmahabub543@gmail.com>
2023-07-10 15:55:24 +0600

    [Modify] /create_blog page

---
+++ components/pages/createBlog/AsideContent.tsx
@@ -0,0 +1,24 @@
+const AsideContent = () => {
+  return (
+    <>
+      <div className="box_area">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit magnam
+        dolorum vitae explicabo illum possimus, magni vero voluptate harum nulla
+        ea rem ex temporibus quisquam unde soluta ducimus fugiat impedit
+        delectus asperiores facere. Sapiente quam adipisci magni vero
+        asperiores! Similique assumenda atque sit, explicabo accusantium
+        nesciunt! Impedit omnis odio reprehenderit?
+      </div>
+      <div className="box_area">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit magnam
+        dolorum vitae explicabo illum possimus, magni vero voluptate harum nulla
+        ea rem ex temporibus quisquam unde soluta ducimus fugiat impedit
+        delectus asperiores facere. Sapiente quam adipisci magni vero
+        asperiores! Similique assumenda atque sit, explicabo accusantium
+        nesciunt! Impedit omnis odio reprehenderit?
+      </div>
+    </>
+  );
+};
+
+export default AsideContent;
--- components/pages/createBlog/CreateBlogBody.tsx
+++ components/pages/createBlog/CreateBlogBody.tsx
@@ -1,12 +1,16 @@
+import BlogAction from "./BlogAction";
 import CreateBlogContent from "./CreateBlogContent";
 
 const CreateBlogBody = ({ renderComponent }: { renderComponent: string }) => {
   return (
     <>
       {renderComponent === "edit" && (
-        <section className="box_area_without_p">
-          <CreateBlogContent />
-        </section>
+        <>
+          <section className="box_area_without_p">
+            <CreateBlogContent />
+          </section>
+          <BlogAction />
+        </>
       )}
       {renderComponent === "preview" && (
         <section className="box_area">preview</section>
